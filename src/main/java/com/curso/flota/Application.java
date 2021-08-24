a65c6b096a9cb3ddbf470e7642b5f2f6dbd89999
begonaOlea <bolea@programia.es>
2021-08-24 11:17:46 +0200

    Inicio
    INICIO

---
+++ src/main/java/com/curso/flota/Application.java
@@ -0,0 +1,46 @@
+package com.curso.flota;
+
+import com.curso.flota.dominio.Vehiculo;
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+		
+		//uso de una clase 
+		//crear una instancia de la clase (objeto)
+		
+		Vehiculo v = new Vehiculo(200.0, "B7777");
+		Vehiculo v2 = new Vehiculo(3000.0,"S8888");
+		Vehiculo v3 = v;
+		
+		// v, v2, v3 son variables de referencia que apuntan a un objeto de 
+		//           la clase Vehiculo
+		
+		
+		int nota = 8;   //nota es una variable de tipos primitivo
+		
+
+		System.out.printf("Antes Carga maxima es %f, y nota %d ", v2.getCargaMaxima(), nota);
+		
+		generador(v2, nota);
+		
+		System.out.printf("Despues Carga maxima es %f, y nota %d ", v2.getCargaMaxima(), nota);
+		
+		
+	}
+	
+	
+	private static void generador(Vehiculo v, int nota) {
+		
+		 Vehiculo v4 = v;
+		 nota = 9;
+		 
+		 v4.setCargaMaxima(555.0);
+		 
+		 Vehiculo v5 = new Vehiculo(4000, "U789");
+		 
+		
+	}
+	
+}
---
+++ src/main/java/com/curso/flota/dominio/Vehiculo.java
@@ -0,0 +1,76 @@
+package com.curso.flota.dominio;
+
+import java.io.Serializable;
+
+/**
+ * Clase que modela los datos de un vehiculo
+ * La carga maxima permitida se asigna en kg
+ * 
+ * @author begonaolea
+ * @version 1.0.0
+ */
+public class Vehiculo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	// atributos
+	private String matricula;
+	private double cargaMaxima;
+	private double cargaActual;
+	private int numCajas;
+
+	//constructores
+	public Vehiculo(double cargaMaxima, String matricula) {
+		super();
+		this.cargaMaxima = cargaMaxima;
+		this.matricula = matricula;
+		this.cargaActual = 0;
+		this.numCajas = 0;
+	}
+	
+	
+	//métodos getters y setters
+	
+
+	public double getCargaMaxima() {
+		return cargaMaxima;
+	}
+	
+
+	public void setCargaMaxima(double cargaMaxima) {
+		this.cargaMaxima = cargaMaxima;
+	}
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+
+	public double getCargaActual() {
+		return cargaActual;
+	}
+
+
+	public void setCargaActual(double cargaActual) {
+		this.cargaActual = cargaActual;
+	}
+
+
+	public int getNumCajas() {
+		return numCajas;
+	}
+
+
+	public void setNumCajas(int numCajas) {
+		this.numCajas = numCajas;
+	}
+	
+	
+	
+}
